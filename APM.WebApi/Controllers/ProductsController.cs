02fbd81de80d5a88218308ca0692731fbd28076d
vn00m8d <vn00m8d@mx.wal-mart.com>
2018-10-31 12:31:45 -0600

    Se inicia con los cambios para recuperar datos

---
+++ APM.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using APM.WebApi.Models;
+using APM.WebAPI.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace APM.WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        public IEnumerable<Product> Get()
+        {
+            var productRepository = new ProductRepository();
+
+            return productRepository.Retrieve();
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
