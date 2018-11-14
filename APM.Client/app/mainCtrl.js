792efe16bd40c5e3246d5523d94aacecdc79b8dd
robertogamez <vn00m8demail.wal/mart.com>
2018-11-14 15:16:50 -0600

    Cambios para integrar el login

--- APM.Client/app/mainCtrl.js
+++ APM.Client/app/mainCtrl.js
@@ -19,7 +19,7 @@
         vm.registerUser = function () {
             vm.userData.confirmPassword = vm.userData.password;
 
-            userAccount.registerUser(vm.userData, function (data) {
+            userAccount.registration.registerUser(vm.userData, function (data) {
                 vm.confirmPassword = '';
                 vm.message = '... Registration successful';
                 vm.login();
@@ -41,7 +41,28 @@
         }
 
         vm.login = function () {
+            vm.userData.grant_type = 'password';
+            vm.userData.userName = vm.userData.email;
 
+            userAccount.login.loginUser(vm.userData, function (data) {
+                vm.isLoggedIn = true;
+                vm.message = '';
+                vm.password = '';
+                vm.token = data.access_token;
+            }, function (response) {
+                vm.isLoggedIn = false;
+                vm.message = response.statusText + "\r\n";
+
+                if (response.data.modelState) {
+                    for (var key in response.data.modelState) {
+                        vm.message += response.data.modelState[key] + "\r\n";
+                    }
+                }
+
+                if (response.data.exceptionMessage) {
+                    vm.message += response.data.exceptionMessage;
+                }
+            })
         }
     }
 
--- APM.Client/common/userAccount.js
+++ APM.Client/common/userAccount.js
@@ -6,11 +6,28 @@
         .factory('userAccount', ['$resource', 'appSettings', userAccount]);
 
     function userAccount($resource, appSettings) {
-        return $resource(appSettings.serverPath + '/api/account/register', null, {
-            'registerUser': {
-                method: 'post'
-            }
-        });
+        return {
+            registration: 
+                $resource(appSettings.serverPath + '/api/account/register', null, {
+                    'registerUser': {
+                        method: 'post'
+                    }
+                }),
+            login: $resource(appSettings.serverPath + '/Token', null, {
+                'loginUser': {
+                    method: 'post',
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                    transformRequest: function (data, headersGetter) {
+                        var str = [];
+                        for (var d in data) {
+                            str.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+                        }
+
+                        return str.join('&');
+                    }
+                }
+            })
+        };
     }
 
 })();
